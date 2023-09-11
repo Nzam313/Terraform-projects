1b6d87efa958cca2f5dcc057b7adbe1b5eb76480
nzam313 <kp_nizam@hotmail.com>
2023-09-11 15:29:59 +0400

    First commit fro ec2 branch

--- main.tf
+++ main.tf
@@ -1,65 +1,34 @@
-provider "aws" {
-    region = "ap-south-1"
-    }
-
-variable "vpc_cidr_block" {
-  type        = string
-  description = "cidr block for vpc"
+provider aws {
+  region = "ap-south-1"
 }
 
-variable "subnet_cidr_block" {
-    type = string
-    description = "cidr block for subnet"
+variable vpc_cidr_block {
+  type = string
+}
+variable subnet_cidr_block {
+  type = string
+}
+variable avail_zone {
+  type = string
+}
+variable environment_prefix {
+  type = string
 }
 
-variable "subnet_cidr_block_default" {
-    type = string
-    description = "cidr block for subnet default"
+resource "aws_vpc" "myapp_vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name : "${var.environment_prefix}-vpc" 
+  }
+
 }
 
-variable environment {
-  description = "Type of environment"
-}
+resource "aws_subnet" "myapp_subnet" {
+  vpc_id = aws_vpc.myapp_vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name : "${var.environment_prefix}-subnet-1"
+  }
 
-
-
-resource "aws_vpc" "development_vpc" {
-    cidr_block = var.vpc_cidr_block
-    tags = {
-        Name: var.environment
-    }
-}
-
-resource "aws_subnet" "development_subnet" {
-    vpc_id = aws_vpc.development_vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = "ap-south-1a"
-    tags =  {
-        Name: var.environment
-    }
 } 
-
-data "aws_vpc" "default_vpc"{
-    default = true
-}
-
-resource "aws_subnet" "development_subnet_1" {
-    vpc_id = data.aws_vpc.default_vpc.id
-    cidr_block = var.subnet_cidr_block_default
-    availability_zone = "ap-south-1a"
-    tags = {
-        Name: var.environment
-    }
-} 
-
-output "vpc_id" {
-  value = aws_vpc.development_vpc.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.development_subnet.id
-}
-
-output "default_subnet_id" {
-  value = aws_subnet.development_subnet_1.id
-}
