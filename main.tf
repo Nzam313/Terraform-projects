4879d6a1450bd42274d0cad358dd6803a71f775a
nzam313 <kp_nizam@hotmail.com>
2023-09-20 15:55:32 +0400

    added security group

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp_vpc" {
 cidr_block = var.vpc_cidr_block
@@ -42,6 +43,39 @@ resource "aws_default_route_table" "main_rtb" {
     }
 }
 
-    
+resource "aws_security_group" "myapp_sg" {
+  name = "myapp_sg"
+  vpc_id = aws_vpc.myapp_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-sg"
+    }
+}
+
+
+
+
 
 
