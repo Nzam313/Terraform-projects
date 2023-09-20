72bc452fa46e822838189b301224a0e6de925662
nzam313 <kp_nizam@hotmail.com>
2023-09-20 10:48:31 +0400

    yml syntax correction

--- main.tf
+++ main.tf
@@ -1,34 +1,26 @@
 provider aws {
-  region = "ap-south-1"
+  region = "ap-south-1" 
 }
 
-variable vpc_cidr_block {
-  type = string
-}
-variable subnet_cidr_block {
-  type = string
-}
-variable avail_zone {
-  type = string
-}
-variable environment_prefix {
-  type = string
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
 resource "aws_vpc" "myapp_vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name : "${var.environment_prefix}-vpc" 
-  }
-
+cidr_block = var.vpc_cidr_block
+tags = {
+  Name = " ${var.env_prefix}-vpc"
+}
 }
 
 resource "aws_subnet" "myapp_subnet" {
-  vpc_id = aws_vpc.myapp_vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name : "${var.environment_prefix}-subnet-1"
-  }
+vpc_id = aws_vpc.myapp_vpc.id
+cidr_block = var.subnet_cidr_block
+availability_zone = var.avail_zone
+tags = {
+  Name = " ${var.env_prefix}-subnet-1"
+}
+}
 
-} 
+
